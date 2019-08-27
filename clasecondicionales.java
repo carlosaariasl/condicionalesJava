a9696a67ca388334c5a922893554abc39d410193
carlosaariasl <43482712+carlosaariasl@users.noreply.github.com>
2019-08-27 10:10:17 -0600

    Add files via upload

---
+++ clasecondicionales.java
@@ -0,0 +1,19 @@
+public class claseIf{
+    public claseIf(){
+        //El constructor
+    }
+    integer num1=0; num2=0;
+
+    public void setIgual(int x, int y){
+        this.num1=x;
+        this.num2=y;
+        if (num1==num2){
+            System.out.println("Los numeros son iguales");
+        }else
+        {
+            System.out.println("Los numeros son distintos");
+        }
+
+    }
+}
+
